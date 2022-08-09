7abce1bc08ffc0f806c5b0244f8667ba2922f60a
ColeCoug <40366361+ColeCoug@users.noreply.github.com>
2022-08-09 00:36:32 -0500

    fixed js

--- mySketch.js
+++ mySketch.js
@@ -15,7 +15,7 @@ function draw() {
 	Shader.setUniform('pal',palette);
 	Shader.setUniform('c',[sin(frameCount/speed)*n,cos(frameCount/speed)*n]);
   rect(0,0,width,height);
-sketch-container
+
   circle(mouseX, mouseY, 50);
 
   let xLabel = document.getElementById('x-label');
